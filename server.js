611a0ddf7226f79e7d2241b7a6f6614b4f44a383
ghuge123 <dipakghuge65@gmail.com>
2026-03-26 09:59:05 +0530

    Updating cors

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173', // your frontend Vite app
+    origin: 'https://social-frontend-psi.vercel.app/', // your frontend Vite app
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
